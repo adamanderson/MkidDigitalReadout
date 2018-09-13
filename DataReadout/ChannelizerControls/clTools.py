212f6478a92abaf52ee21d8ab8aae3fb5f326b38
Chris Stoughton <chris.stoughton@gmail.com>
2018-09-13 13:01:03 -0500

    add functions

--- DataReadout/ChannelizerControls/clTools.py
+++ DataReadout/ChannelizerControls/clTools.py
@@ -15,7 +15,7 @@ and if you make changes to code in this file:
 > reload(clTools)
 
 """
-import os, sys, warnings
+import os, sys, warnings, datetime, pickle
 import numpy as np
 import datetime, socket
 import ConfigParser
@@ -48,7 +48,7 @@ def loadFIRs(rchc):
     rchc.loadFIRs()
 
 
-def performIQSweep(rchc):
+def performIQSweep(rchc, saveToFile=None):
     LO_freq = rchc.roachController.LOFreq
     LO_span = rchc.config.getfloat(rchc.roachString,'sweeplospan')
     LO_step = rchc.config.getfloat(rchc.roachString,'sweeplostep')
@@ -57,9 +57,49 @@ def performIQSweep(rchc):
     iqData = rchc.roachController.performIQSweep(LO_start/1.e6,
                                                  LO_end/1.e6,
                                                  LO_step/1.e6,
-                                                 verbose=True)
+                                                 verbose=False)
+    iqData['LO_freq']   = LO_freq
+    iqData['LO_span']   = LO_span
+    iqData['LO_step']   = LO_step
+    iqData['LO_start']  = LO_start
+    iqData['LO_end']    = LO_end
+    iqData['atten1']    = rchc.roachController.attenVal[1]
+    iqData['atten2']    = rchc.roachController.attenVal[2]
+    iqData['atten3']    = rchc.roachController.attenVal[3]
+    iqData['atten4']    = rchc.roachController.attenVal[4]
+    iqData['timestamp'] = datetime.datetime.now()
+    iqData['freqList']  = rchc.roachController.freqList
+    
+    if saveToFile is not None:
+        print "save to file"
+        saveIQSweepToFile(rchc, iqData, saveToFile)
     return iqData
 
+def takeLoopData(rchc, fnPrefix):
+    iFile = 0
+    while True:
+        pklFileName = "%s-%04d.pkl"%(fnPrefix,iFile)
+        handle = open(pklFileName,'wb')
+        for i in range(100):
+            iqData = performIQSweep(rchc)
+            pickle.dump(iqData, handle)
+            print "%s %5d %s"%(pklFileName, i, str(iqData['timestamp']))
+        handle.close()
+        iFile += 1
+        
+def saveIQSweepToFile(rchc, iqData, saveToFile):
+    print "saveToFile =",saveToFile
+    freqList = rchc.roachController.freqList
+    attenList = rchc.roachController.attenList
+    for iFreq,f0 in enumerage(freqList):
+        w = iqsweep.IQSweep()
+        w.f0 = freqList[iFreq]
+        w.span = iqData['LO_span']/1e6
+        w.fsteps = len(iqData['freqOffsets'])
+        # attempt to copy logic in RoachStateMachein after the "save the power sweep" comment
+        w.atten1 = attenList[iFreq] + iqData['atten3']
+        w.atten2 = attenList[iFreq] + iqData['atten4']
+        
 def readPhasesTest(rchc):
     freqChan = 0
     duration = 1.0
@@ -97,8 +137,14 @@ def doOnePhaseSnapshot(rchc, freqChan, duration, outDir,fileName, format="ascii"
     if format is None :
 		format=="ascii"
 
-    wp=WritePhaseData.writePhaseData(filename,format,freqChan,freqs,duration,data)
-    wp.Write()
+            
+    wp=WritePhaseData.WritePhaseData(filename,format,freqChan,freqs,duration,data)
+    ##wp.write()
 	   
     return data
 
+def takePhaseData(rchc, nToDo, freqChan, duration, fileNamePrefix):
+    for i in range(nToDo):
+        fileName = "%s-%04d"%(fileNamePrefix,i)
+        print i, fileName
+        doOnePhaseSnapshot(rchc, freqChan, duration, ".", fileName, format='hdf5') 
